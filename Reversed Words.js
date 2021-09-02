4f5e83f05562db4d13a9d82ca121627bf032e21e
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-09-02 10:17:55 -0300

    Update Reversed Words.js

--- Reversed Words.js
+++ Reversed Words.js
@@ -4,3 +4,6 @@
 
 "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
 
+function reverseWords(str){
+  return str.split(" ").reverse().join(" "); // reverse those words
+}
