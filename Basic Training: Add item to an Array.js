1064c5c05194df9fb4258cc7657d4f93e4212295
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-09-04 09:28:49 -0300

    Update Basic Training: Add item to an Array.js

--- Basic Training: Add item to an Array.js
+++ Basic Training: Add item to an Array.js
@@ -5,3 +5,5 @@ After your code executes the websites array should == ["codewars"]
 The websites array has already been defined for you using the following code:
 var websites = [];
 */
+
+websites.push('codewars');
