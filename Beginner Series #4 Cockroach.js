a274cf4eb35c0601749514af75179f93c1f6f3cd
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-09-06 23:35:59 -0300

    Update Beginner Series #4 Cockroach.js

--- Beginner Series #4 Cockroach.js
+++ Beginner Series #4 Cockroach.js
@@ -6,3 +6,5 @@ For example:
 cockroachSpeed(1.08) == 30
 Note! The input is a Real number (actual type is language dependent) and is >= 0. The result should be an Integer.
 */
+
+const cockroachSpeed = s => Math.floor(s / 0.036);
