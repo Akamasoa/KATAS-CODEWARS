4576d94f193b2e6408996de23c6d93f2b00d3206
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-09-11 10:02:10 -0300

    Update Triple Trouble.js

--- Triple Trouble.js
+++ Triple Trouble.js
@@ -1,1 +1,9 @@
+/*
+Title:
+  Triple Trouble
+
+Description:
+  Create a function that will return a string that combines all of the letters of the three inputed strings in groups.
+  Taking the first letter of all of the inputs and grouping them next to each other.
+  Do this for every letter, see example below!
 
