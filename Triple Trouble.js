28f943cb0679beb71f2330734f103353eac80842
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-09-11 10:04:23 -0300

    Update Triple Trouble.js

--- Triple Trouble.js
+++ Triple Trouble.js
@@ -7,3 +7,21 @@ Description:
   Taking the first letter of all of the inputs and grouping them next to each other.
   Do this for every letter, see example below!
 
+Examples:
+    tripleTrouble('aa', 'bb', 'cc')
+      // => "abcabc"
+  
+Notes:
+    You can expect all of the inputs to be the same length.
+*/
+
+// Long Solution:
+
+const tripleTrouble = (one, two, three) => {
+  let string = ''
+  for (let i = 0; i < one.length; i++) {
+    string += `${one[i]}${two[i]}${three[i]}`
+  }
+  return string
+}
+
