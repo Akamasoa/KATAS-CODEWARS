d94b599d1c1d8c62ced999ed9fc57c3bea3d7c9b
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-09-12 20:26:05 -0300

    Update Barking mad.js

--- Barking mad.js
+++ Barking mad.js
@@ -42,3 +42,17 @@ scoobydoo.bark();
 
 // Function Export
 module.exports = Dog
+
+//Test:
+
+const Dog = require('.')
+
+test('Test 1', () => {
+  const snoopy = new Dog('Beagle')
+  expect(snoopy.bark()).toBe('Woof')
+})
+
+test('Test 2', () => {
+  const scoobyDoo = new Dog('Great Dane')
+  expect(scoobyDoo.bark()).toBe('Woof')
+})
