9a0eb9d9343cccaea65385e20e0d2d6fe9107e9a
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-09-12 20:13:02 -0300

    Update Barking mad.js

--- Barking mad.js
+++ Barking mad.js
@@ -5,3 +5,9 @@ Title:
 Description:
   Teach snoopy and scooby doo how to bark using object methods.
   Currently only snoopy can bark and not scooby doo.
+
+Examples:
+    snoopy.bark(); // return "Woof"
+    scoobydoo.bark(); // undefined
+Notes:
+    Use method prototypes to enable all Dogs to bark.
