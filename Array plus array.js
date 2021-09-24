f08b2c2164b917abeff4a5d64838e2f032c80da2
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-09-23 23:04:44 -0500

    Update Array plus array.js

--- Array plus array.js
+++ Array plus array.js
@@ -1,1 +1,15 @@
-
+/*
+  Title:
+    Array plus array
+  Description:
+    I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements.
+    I'll appreciate for your help.
+  Notes:
+    Each array includes only integer numbers. Output is a number too.
+  Kata Link:
+    https://www.codewars.com/kata/array-plus-array
+  Discuss Link:
+    https://www.codewars.com/kata/array-plus-array/discuss
+  Solutions Link:
+    https://www.codewars.com/kata/array-plus-array/solutions
+*/
