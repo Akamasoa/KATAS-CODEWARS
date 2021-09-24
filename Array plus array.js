46aeda5791a67b6b3edd6fc1401ad3993d3a46c3
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-09-23 23:06:18 -0500

    Update Array plus array.js

--- Array plus array.js
+++ Array plus array.js
@@ -21,3 +21,23 @@ const arrayPlusArray = (array1, array2) =>
 
 // Function Export
 module.exports = arrayPlusArray
+
+//Test:
+
+const arrayPlusArray = require('.')
+
+test('Test 1', () => {
+  expect(arrayPlusArray([1, 2, 3], [4, 5, 6])).toBe(21)
+})
+
+test('Test 2', () => {
+  expect(arrayPlusArray([-1, -2, -3], [-4, -5, -6])).toBe(-21)
+})
+
+test('Test 3', () => {
+  expect(arrayPlusArray([0, 0, 0], [4, 5, 6])).toBe(15)
+})
+
+test('Test 4', () => {
+  expect(arrayPlusArray([100, 200, 300], [400, 500, 600])).toBe(2100)
+})
