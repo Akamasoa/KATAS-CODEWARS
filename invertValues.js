2c79c319a55fe1a762538e1bb4e5e737e3e683a9
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-09-24 00:41:49 -0300

    Update invertValues.js

--- invertValues.js
+++ invertValues.js
@@ -5,12 +5,11 @@
 //invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
 
 const invert = (array) => {
-	const arrayNew = [];
-	array.forEach((elemn) => {
-		if ((elemn) => 0 || elemn <= 0) {
-			arrayNew.push(elemn * -1);
-		}
-	});
-
-	return arrayNew;
+    const arrayNew = [];
+    array.forEach((elemn) => {
+	if ((elemn) => 0 || elemn <= 0) {
+		arrayNew.push(elemn * -1);
+	}
+     });
+   return arrayNew;
 };
