6ac2da9fc102e42617481e150a311ed8b408576c
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-09-26 22:26:27 -0300

    Update Lost Numbers.js

--- Lost Numbers.js
+++ Lost Numbers.js
@@ -7,3 +7,5 @@ Surprisingly I couldn't find it on Codewars so now its here for everyone. If you
 through this tutorial http://www.codecademy.com/courses/functions-in-javascript-2-0/0/1?curriculum_id=4f4bdd96848740000300026a
 */
 
+var lost = [4, 8, 15, 16, 23, 42];
+const isLost=(n)=>lost.includes(n);
