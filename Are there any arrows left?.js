4f7738eb737bebd53e3a258afc12ee9c8f024f99
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-10-04 23:32:40 -0300

    Update Are there any arrows left?.js

--- Are there any arrows left?.js
+++ Are there any arrows left?.js
@@ -7,5 +7,4 @@ You need to verify that you have some good ones left, in order to prepare for ba
 anyArrows([{range: 5}, {range: 10, damaged: true}, {damaged: true}])
 If an arrow in the quiver does not have a damaged status, it means it's new.
 The expected result is a boolean, indicating whether you have any good arrows left.
-Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
 */
