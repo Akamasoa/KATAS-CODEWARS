3ff1bda808431dee00442b2bacc43e07fa319314
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-10-28 17:58:44 -0300

    Create It's Full of Stars.js

--- It's Full of Stars.js
+++ It's Full of Stars.js
@@ -1,1 +1,18 @@
+/*
+Description:
+You need to create rectangles of stars for your next great text-based adventure game.
+Each rectangle is made of rows and columns of asterisks. The following piece of code creates a rectangle with 3 rows and 6 columns:
+var yay = printStars(3, 6);
+console.log(yay);
+This example code above would print out the following:
+******
+******
+******
+In this example, the string returned from the function is the following:
+"******\n******\n******"
+A few additional rules:
+A newline character ( "\n" ) should be between each row of asterisks.
+If the number of rows or columns is 0 or less, return an empty string ( "" ).
+Good luck!
+*/
 
