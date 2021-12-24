f86cc409c0a16350e6f31479b7e8c1ee3be51afe
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-12-23 23:35:37 -0300

    Update Between a and b.js

--- Between a and b.js
+++ Between a and b.js
@@ -5,3 +5,12 @@ For example:
 product(2,5) => 3 * 4 = 12
 product(5,2) => 'Error' / Exception
 */
+
+function product(a,b){
+  let arr = [];
+  for (let i=a+1;i<b;i++){
+  arr.push(i)
+  }
+  if (!arr.length) return 'Error'
+  return arr.reduce((a,b)=>a*b,1)
+}
