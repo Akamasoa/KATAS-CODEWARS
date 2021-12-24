5582d6a9db5d6f9af41392c362197434da2082d5
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-12-24 17:47:21 -0300

    Update Complete the code.js

--- Complete the code.js
+++ Complete the code.js
@@ -4,4 +4,6 @@ The age of driving is 18 at minimum.
 Create a function that checks 'age' value and return true if it is 18 or bigger If not, return false.
 */
 
-
+function canYouDrive(age) {
+  return age>=18;
+}
