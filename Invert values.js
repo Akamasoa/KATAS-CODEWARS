8dad87647550489c9324a579eb5631a307088e18
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2021-12-28 22:56:23 -0300

    Update Invert values.js

--- Invert values.js
+++ Invert values.js
@@ -6,3 +6,7 @@ invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
 invert([]) == []
 You can assume that all values are integers.
 */
+
+function invert(array) {
+   return array.map( x => x === 0 ? x : -x);
+}
