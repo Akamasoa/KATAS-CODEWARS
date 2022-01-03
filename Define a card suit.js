56d01f4ba38eb0463f990fc6b28e568cac958098
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2022-01-02 22:19:26 -0300

    Update Define a card suit.js

--- Define a card suit.js
+++ Define a card suit.js
@@ -1,1 +1,23 @@
+/*
+  Title:
+    Define a card suit
+  Description:
+    You get any card as an argument. Your task is to return a suit of this card.
+    Our deck (is preloaded):
+    deck = ['2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣','A♣',
+            '2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦','A♦',
+            '2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥','A♥',
+            '2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠','A♠'];
+    ('3♣') -> return 'clubs'
+    ('3♦') -> return 'diamonds'
+    ('3♥') -> return 'hearts'
+    ('3♠') -> return 'spades'
+  Kata Link:
+    https://www.codewars.com/kata/define-a-card-suit
+  Discuss Link:
+    https://www.codewars.com/kata/define-a-card-suit/discuss
+  Solutions Link:
+    https://www.codewars.com/kata/define-a-card-suit/solutions
+*/
+
 
