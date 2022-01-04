62f4b104174ab8c61cd2d326b93066df278bf0ea
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2022-01-03 22:27:58 -0300

    Create Shifty Closures.js

---
+++ Shifty Closures.js
@@ -0,0 +1,14 @@
+/*
+  Title:
+    Shifty Closures
+  Description:
+    Functional closures can get overly attached. Set them straight!
+    Why doesn't greet_abe() actually greet Abe?
+  Kata Link:
+    https://www.codewars.com/kata/shifty-closures
+  Discuss Link:
+    https://www.codewars.com/kata/shifty-closures/discuss
+  Solutions Link:
+    https://www.codewars.com/kata/shifty-closures/solutions
+*/
+
