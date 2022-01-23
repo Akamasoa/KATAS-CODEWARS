4e9cf2b8af1120c8341edd7cee163ea0e83806c4
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2022-01-22 22:00:40 -0300

    Update Implement a Filter function.js

--- Implement a Filter function.js
+++ Implement a Filter function.js
@@ -6,4 +6,13 @@ The usage is quite simple, like:
 should output [4]
 */
 
+Array.prototype.filter = function(fn) {
+  const newArray = [];
+
+  for (let i = 0; i < this.length; i++) {
+    if (fn(this[i])) newArray.push(this[i]);
+  }
+
+  return newArray;
+}
 
