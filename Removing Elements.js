5538cae1997a69a2b230e27895d20407bd8d426f
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2022-01-24 22:03:12 -0300

    Create Removing Elements.js

---
+++ Removing Elements.js
@@ -0,0 +1,12 @@
+/*
+Title:
+  Removing Elements
+  
+Description:
+  Take an array and remove every second element out of that array.
+  Always keep the first element and start removing with the next element.
+
+Examples:
+  myArr = ['Keep', 'Remove', 'Keep', 'Remove', 'Keep', ...];
+*/
+
