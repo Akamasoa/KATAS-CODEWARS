b568a598e7fd8f2ca15214bf5208c20691cae24d
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2022-01-25 21:27:14 -0300

    Create Vowel remover.js

---
+++ Vowel remover.js
@@ -0,0 +1,17 @@
+/*
+  Title:
+    Vowel remover
+    
+  Description:
+    Create a function called shortcut to remove all the lowercase vowels in a given string.
+    
+  Examples:
+    shortcut("codewars")
+      // --> cdwrs
+    shortcut("goodbye")
+      // --> gdby
+  Notes:
+    Don't worry about uppercase vowels
+*/
+
+
