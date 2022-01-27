eab562f074db2d40b940d6927d860a168572b7dc
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2022-01-26 23:41:09 -0300

    Create Sort and Star.js

---
+++ Sort and Star.js
@@ -0,0 +1,10 @@
+/*
+  Title:
+    Sort and Star
+    
+  Description:
+    You will be given an vector of string(s).
+    You must sort it alphabetically (case-sensitive!!) and then return the first value.
+    The returned value must be a string, and have '***' between each of its letters.
+*/
+
