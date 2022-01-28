167f061c9b0391138b5adf54520b3eef83062c3f
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2022-01-27 23:15:45 -0300

    Update Max Headroom and JavaScript style.js

--- Max Headroom and JavaScript style.js
+++ Max Headroom and JavaScript style.js
@@ -7,3 +7,5 @@
     Can you spot a problem and fix it? Can you learn something about JavaScript style in this kata?
 */
 
+const getMax1 = () => ({ name: 'Max Headroom' });
+const getMax2 = () => ({ name: 'Max Headroom' });
