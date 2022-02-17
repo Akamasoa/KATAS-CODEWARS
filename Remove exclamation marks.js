fa50f31c1095a170792e602545317c0952e393bc
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2022-02-16 22:05:21 -0300

    Update Remove exclamation marks.js

--- Remove exclamation marks.js
+++ Remove exclamation marks.js
@@ -3,3 +3,6 @@ Description:
 Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
 */
 
+function removeExclamationMarks(s) {
+  return s.replace(/!/g,'');
+}
