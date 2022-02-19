4ac710c17470793653e73f54c1d18bd276eec001
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2022-02-18 21:46:42 -0300

    Update How old will I be in 2099?.js

--- How old will I be in 2099?.js
+++ How old will I be in 2099?.js
@@ -7,3 +7,10 @@ Provide output in this format: For dates in the future: "You are ... year(s) old
 Good Luck!
 */
 
+function  calculateAge(a,b) {
+  if (a>b&&+a-b==1)return `You will be born in ${+a-b} year.`;
+  else if (a<b&&+b-a==1)return `You are ${b-a} year old.`;
+  else if (a>b)return `You will be born in ${+a-b} years.`;
+  else if (a<b) return `You are ${b-a} years old.`;
+  return 'You were born this very year!';
+}
