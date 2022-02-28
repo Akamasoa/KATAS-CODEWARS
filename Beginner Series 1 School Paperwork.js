c9789585cc6a9f41d8d5f52a8bfd7cdc64722aa2
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2022-02-28 20:30:39 -0300

    Create Beginner Series 1 School Paperwork.js

---
+++ Beginner Series 1 School Paperwork.js
@@ -0,0 +1,16 @@
+/*
+Title:
+Beginner Series #1 School Paperwork
+
+Description:
+Your classmates asked you to copy some paperwork for them.
+You know that there are 'n' classmates and the paperwork has 'm' pages.
+Your task is to calculate how many blank pages do you need.
+
+Examples:
+paperwork(5, 5) == 25
+
+Notes:
+If n or m < 0 return 0!
+*/
+
