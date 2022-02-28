49a5c6a09821d43611cd337e536014a1f088694c
Franco Sebastián Genre <84336216+Akamasoa@users.noreply.github.com>
2022-02-28 20:39:58 -0300

    Create Last.js

---
+++ Last.js
@@ -0,0 +1,19 @@
+/*
+Title:
+Last
+
+Description:
+Find the last element of a list.
+
+Examples:
+last( [1,2,3,4] ) // => 4
+last( "xyz" )     // => z
+last( 1,2,3,4 )   // => 4
+
+Notes:
+In javascript and CoffeeScript a list will be
+  * an array
+  * a string
+  * a list of arguments
+*/
+
