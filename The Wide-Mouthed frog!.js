5e72c9428cf83b517d4106d23d5e3f1544dae68d
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-03-16 23:00:13 -0300

    Update The Wide-Mouthed frog!.js

--- The Wide-Mouthed frog!.js
+++ The Wide-Mouthed frog!.js
@@ -5,3 +5,8 @@ He just can't stop asking the creatures he encounters what they like to eat. But
 When he meets the alligator, it then makes a tiny mouth.
 Your goal in this kata is to create complete the mouth_size method this method take one argument animal which corresponds to the animal encountered by frog. If this one is an alligator (case insensitive) return small otherwise return wide.
 */
+
+
+function mouthSize(animal) {
+ return /alligator/i.test(animal)?'small':"wide";
+}
