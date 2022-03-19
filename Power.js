a8e179222388a2a405d08e8254badc191a879f97
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-03-18 22:14:36 -0300

    Update Power.js

--- Power.js
+++ Power.js
@@ -7,3 +7,13 @@ numberToPower(2,3)  // -> 8 ( = 2 * 2 * 2 )
 numberToPower(10,6) // -> 1000000
 Note: and some other functions like and are disabled.Math.powMatheval()**
 */
+
+function numberToPower(number, power){
+  let result = 1;
+  for (let i = 1; i <= power; i++){
+    result *= number;
+  }
+  return result;
+}
+
+https://www.codewars.com/kata/562926c855ca9fdc4800005b/solutions/javascript
