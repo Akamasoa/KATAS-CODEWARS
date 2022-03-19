001605b885476a724d35a81e6e21d4acc7032974
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-03-18 22:13:34 -0300

    Create Power.js

---
+++ Power.js
@@ -0,0 +1,9 @@
+/*
+The goal is to create a function '' that "raises" the up to (ie multiplies by itself times).numberToPower(number, power)numberpowernumberpower
+
+Examples
+numberToPower(3,2)  // -> 9 ( = 3 * 3 )
+numberToPower(2,3)  // -> 8 ( = 2 * 2 * 2 )
+numberToPower(10,6) // -> 1000000
+Note: and some other functions like and are disabled.Math.powMatheval()**
+*/
