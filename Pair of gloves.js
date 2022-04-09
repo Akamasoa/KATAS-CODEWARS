ce955b0eaaf5905d16cf6fc3aefbd06849bdfe95
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-04-09 16:51:31 -0300

    Update Pair of gloves.js

--- Pair of gloves.js
+++ Pair of gloves.js
@@ -13,3 +13,14 @@ input = ["red", "red", "red", "red", "red", "red"]
 result = 3 (3 red pairs)
 */
 
+function numberOfPairs(gloves){
+  var glovesObj = gloves.reduce((acc, el)=> {
+    acc[el] = (acc[el] || 0) + 1;
+    return acc;
+  }, {})
+  let r = 0;
+  for(color in glovesObj){
+    r+=Math.floor(glovesObj[color] / 2)
+  }
+  return r;
+}
