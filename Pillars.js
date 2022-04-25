d6682862b5df794ba20ccc1887ea8da26b39da78
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-04-25 17:31:26 -0300

    Update Pillars.js

--- Pillars.js
+++ Pillars.js
@@ -8,3 +8,7 @@ width of the pillar (10 - 50 centimeters).
 Calculate the distance between the first and the last pillar in centimeters (without the width of the first and last pillar).
 */
 
+function pillars(num_pill, dist, width) {
+  // your code here
+  return num_pill>1?(num_pill-1)*dist*100+(num_pill-2)*width:0;
+}
