74848f83f3a7d41ffc6a91a233ffc30f92e3fd30
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-08-02 23:37:44 -0300

    Update Find the position!.js

--- Find the position!.js
+++ Find the position!.js
@@ -9,3 +9,7 @@ Ouput :: "Position of alphabet: 1"
 This kata is meant for beginners. Rank and upvote to bring it out of beta
 */
 
+function position(letter){
+//Write your own Code!
+  return `Position of alphabet: ${letter.charCodeAt()-96}`;
+}
