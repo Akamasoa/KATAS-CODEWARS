9fadc790bca59d11646473e77f19a181f99e4e33
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-08-04 00:01:09 -0300

    Update Find the first non-consecutive number.js

--- Find the first non-consecutive number.js
+++ Find the first non-consecutive number.js
@@ -22,3 +22,18 @@ Julia: nothing
 Nim: none(int) (See options)
 */
 
+function firstNonConsecutive(arr) {
+  const result = arr.find((number, index) => number !== index + arr[0]);
+  return Number.isInteger(result) ? result : null;
+}
+
+/*
+function firstNonConsecutive(arr) {
+  for (let i = 0; i < arr.length - 1; ++i) {
+    if (arr[i] + 1 !== arr[i + 1]) {
+      return arr[i + 1];
+    }
+  }
+  return null;
+}
+*/
