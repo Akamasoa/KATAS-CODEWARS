f1cc28a38f767628c5e216e95a0954b56cf8effe
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-08-06 23:48:39 -0300

    Update Drink about.js

--- Drink about.js
+++ Drink about.js
@@ -34,3 +34,10 @@ function peopleWithAgeDrink(old) {
     return "drink beer";
   }
 };
+
+/*
+const peopleWithAgeDrink = (age) =>
+  age < 14 ? "drink toddy" :
+  age < 18 ? "drink coke" :
+  age < 21 ? "drink beer" : "drink whisky"
+*/
