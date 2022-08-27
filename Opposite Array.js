aa1e7a362b70e07181bcecb6527c8eaf51ed3e09
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-08-26 22:31:52 -0300

    Create Opposite Array.js

---
+++ Opposite Array.js
@@ -0,0 +1,8 @@
+/*
+DESCRIPTION:
+Given an array of numbers, create a function called oppositeArray that returns an array of numbers that are the additive inverse (opposite or negative) of the original. If the original array is empty, return it.
+
+Ruby/Python: function name will be opposite_list
+Haskell: function name will be opps
+*/
+
