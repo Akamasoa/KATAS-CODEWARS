382b0ad915f1034439ef0f0e41cf93bb7574c52a
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-09-14 23:07:20 -0300

    Create Pre-FizzBuzz Workout #2.js

---
+++ Pre-FizzBuzz Workout #2.js
@@ -0,0 +1,14 @@
+/*
+Description:
+This is the second step to understanding FizzBuzz.
+
+Your inputs: a positive integer, n, greater than or equal to one. n is provided, you have NO CONTROL over its value.
+Your expected outputs: n, "Fizz", "Buzz", or "FizzBuzz" depending on the following rules:
+
+Multiples of 5 and 3 return "FizzBuzz"
+Multiples of 3 return "Fizz"
+Multiples of 5 return "Buzz"
+
+Your job is to write an algorithm that receives the input and outputs the correct value.
+*/
+
