1ac7c8bd3c48e44c4e90250242e91ec6b3a014d5
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-09-26 22:58:47 -0300

    Update and rename A bugs trilogy_ Episode 1 - _Let Math.Random(); decide your future_.js to A bugs trilogy: Episode 1 - "Let Math.Random(); decide your future".js

--- A bugs trilogy: Episode 1 - "Let Math.Random(); decide your future".js
+++ A bugs trilogy: Episode 1 - "Let Math.Random(); decide your future".js
@@ -19,5 +19,12 @@ This code is a mess! Would you help Pac to fix the code in time?
 (This might be helpful -> Math.Random();)
 */
 
-
-
+function yourFutureCareer(){
+	var career = Math.random();
+		if (career <= 0.32) {
+      return 'FrontEnd Developer';
+    } else if (career <= 0.65) {
+      return 'BackEnd Developer';
+    } else {
+      return 'Full-Stack Developer';
+    }
