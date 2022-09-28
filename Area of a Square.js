c6465ac62f7a5bd13e11dacc06b4060e198bea08
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-09-27 23:29:00 -0300

    Create Area of a Square.js

---
+++ Area of a Square.js
@@ -0,0 +1,10 @@
+/*
+Description:
+Write the function squareArea(A) (square_area($A) in PHP) that finds the area of the red square when you have the length of the circular arc A.
+
+alt text
+Use π = Math.PI (M_PI in PHP)
+
+Round to two decimals.
+*/
+
