bcbf6681ec207719ae1fa95ab2632dce6691c286
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-10-03 23:44:41 -0300

    Update CTCI 1.1 Is Unique.js

--- CTCI 1.1 Is Unique.js
+++ CTCI 1.1 Is Unique.js
@@ -3,3 +3,6 @@ Instructions:
 Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
 */
 
+const isUnique = str => {
+   return str.length === [...new Set(str)].length;
+}
