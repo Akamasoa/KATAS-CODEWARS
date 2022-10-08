fd47a5e9783cfc0bb8fa006da806c836ba531f5f
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-10-07 22:06:55 -0300

    Create Find a number increase.js

---
+++ Find a number increase.js
@@ -0,0 +1,5 @@
+/*
+Description:
+A number increased by 7 equals 22. Write a function that return A number's value. Return null if initial_value or final_value is not a number and when final_value is less than initial_value.
+*/
+
