5664f9182eadc31c8d54e54235f464fba60eb09f
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-10-09 20:23:47 -0300

    Update Fun with ES6 Classes #1 - People, people, people.js

--- Fun with ES6 Classes #1 - People, people, people.js
+++ Fun with ES6 Classes #1 - People, people, people.js
@@ -14,3 +14,14 @@ You may use any valid syntax you like; however, it is highly recommended that yo
 Have fun! :D
 */
 
+class Person {
+  constructor(firstName = "John", lastName = "Doe", age = 0, gender = "Male") {
+    Object.assign(this, { firstName, lastName, age, gender });
+  }
+  sayFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+  static greetExtraTerrestrials(raceName) {
+    return `Welcome to Planet Earth ${raceName}`;
+  }
+}
