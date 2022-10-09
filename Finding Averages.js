d2ff8285279e21300ba79c77859306dd6ff6df87
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-10-08 22:09:42 -0300

    Update Finding Averages.js

--- Finding Averages.js
+++ Finding Averages.js
@@ -7,3 +7,7 @@ Make a program that takes a list of a random amount (but will always have atleas
 Ex: If input is [70, 70, 70, 70, 70] the program should return 70 (for obvious reasons)
 */
 
+const average = (n) => {
+   if (Array.isArray(n) && n.every(v => typeof v ==='number')) return n.reduce((a,b) => a + b,0) /n.length;
+   return 'Incorrect';
+}
