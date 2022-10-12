d26f0e8c3c4b1235af6874fdd35c5eec2ff1820f
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-10-11 21:43:27 -0300

    Update Function 3 - multiplying two numbers.js

--- Function 3 - multiplying two numbers.js
+++ Function 3 - multiplying two numbers.js
@@ -1,5 +1,10 @@
 /*
 Description:
-This function has to be called multiply and needs to take two numbers as arguments, and has to return the multiplication of the two arguments.
+Implement a function called multiply, which takes two numbers and returns their product:
+
+multiply(2, 3) = 6
+multiply(0, 188) = 0
+multiply(85, 144) = 12240
 */
 
+let multiply = (a, b) => a * b;
