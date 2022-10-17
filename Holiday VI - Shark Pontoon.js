380707433fbbd890ee05a9b925dd31f7f3fda2f9
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-10-16 22:55:42 -0300

    Update Holiday VI - Shark Pontoon.js

--- Holiday VI - Shark Pontoon.js
+++ Holiday VI - Shark Pontoon.js
@@ -22,3 +22,9 @@ The pontoon, you, and the shark are all aligned in one dimension.
 If you make it, return "Alive!", if not, return "Shark Bait!".
 */
 
+function shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin){
+  if(dolphin){
+    sharkSpeed /= 2;
+  }
+  return pontoonDistance/youSpeed < sharkDistance/sharkSpeed ? "Alive!" : "Shark Bait!";
+}
