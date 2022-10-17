cd2d746ee07dd7a49ebe52ac87d1c77f33fcdae2
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-10-16 23:19:39 -0300

    Update Holiday VIII - Duty Free.js

--- Holiday VIII - Duty Free.js
+++ Holiday VIII - Duty Free.js
@@ -8,3 +8,6 @@ For example, if a bottle cost £10 normally and the discount in duty free was 10
 All inputs will be integers. Please return an integer. Round down.
 */
 
+function dutyFree(normPrice, discount, hol){
+ return Math.floor(hol / (normPrice * discount / 100));
+}
