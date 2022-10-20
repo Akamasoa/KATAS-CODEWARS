a434621af0abfb05bdc06acc5b17252a0be3a63b
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-10-19 23:41:52 -0300

    Update How well did they score.js

--- How well did they score.js
+++ How well did they score.js
@@ -8,3 +8,8 @@ Remember not all fullstops equal the end of a sentence. If there are 3 or 4 in a
 ... Also, be sure to let the students know if they’re missing their answer.
 */
 
+function punctuation(str){
+  if (str.length===0) return 'missing answer';
+  let n = str.match(/\. [A-Z]/g,'')===null?0:str.match(/\. [A-Z]/g,'').length;
+return n>0?'full marks':'no marks';
+}
