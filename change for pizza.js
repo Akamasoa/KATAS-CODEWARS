4e36355b5bea84d488745d6b9fefa27670ad6e3f
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-10-27 23:06:08 -0300

    Update change for pizza.js

--- change for pizza.js
+++ change for pizza.js
@@ -5,3 +5,6 @@ My friend Sergio visits a pizzeria every Tuesday and buys a pizza. Sergio always
 The prices sometimes changes and the pizzeria needs a special program to calculate the amount of money they need to return. The pizzeria's owner studied qbasic in college so he wrote javascript as qbasic. Please, help him and fix a function that calculates the change for pizza.
 */
 
+function rez(price){
+  return 200 - price;
+}
