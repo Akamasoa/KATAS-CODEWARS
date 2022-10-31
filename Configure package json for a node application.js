a273acee0b63459ddbfc799ea7c80baa9869c406
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-10-30 22:33:27 -0300

    Update Configure package json for a node application.js

--- Configure package json for a node application.js
+++ Configure package json for a node application.js
@@ -49,3 +49,10 @@ UNLICENSED
 SEE LICENSE IN filename
 */
 
+const configuration = {
+"name": "your-pack-name",
+  "version": "1.1.1",
+  "description": ""
+};
+// Hint: try creating with `npm init` command
+// Help! -> https://docs.npmjs.com/files/package.json
