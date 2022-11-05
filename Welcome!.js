63daa19e144e6522ffdfd04bf922cf1070192f1f
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-11-04 22:22:34 -0300

    Update Welcome!.js

--- Welcome!.js
+++ Welcome!.js
@@ -30,3 +30,25 @@ IP_ADDRESS_NOT_FOUND - ip address not in the database
 IP_ADDRESS_REQUIRED - no ip address was supplied
 */
 
+function greet(language) {
+	const base ={
+english: 'Welcome',
+czech: 'Vitejte',
+danish: 'Velkomst',
+dutch: 'Welkom',
+estonian: 'Tere tulemast',
+finnish: 'Tervetuloa',
+flemish: 'Welgekomen',
+french: 'Bienvenue',
+german: 'Willkommen',
+irish: 'Failte',
+italian: 'Benvenuto',
+latvian: 'Gaidits',
+lithuanian: 'Laukiamas',
+polish: 'Witamy',
+spanish: 'Bienvenido',
+swedish: 'Valkommen',
+welsh: 'Croeso'
+}
+  return base[language] || base.english;
+}
