5dfe0377a9c48bddcb2bef9c03f818cbdc120408
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-11-04 22:28:33 -0300

    Create Well Done Multiplus.js

---
+++ Well Done Multiplus.js
@@ -0,0 +1,11 @@
+/*
+Description:
+Basics, when you get a multiple of 2, you must return a string "Well". When you get a multiple of 3, you must return a string "Done". And of course, when you get a multiple of 2 and 3, you must return "Well Done".
+
+Ps. Numbers that do not correspond don't needs to add anything.
+
+The output must be an array of strings.
+
+Input Example [2,3,5,6] Output Exemple ["Well", "Done", "Well Done"]
+*/
+
