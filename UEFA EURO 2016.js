0fca426514f4f3e0e7384cde87395736a3c8ef36
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-11-08 11:16:02 -0300

    Update UEFA EURO 2016.js

--- UEFA EURO 2016.js
+++ UEFA EURO 2016.js
@@ -7,3 +7,14 @@ uefaEuro2016(['Belgium', 'Italy'],[0, 2]) // "At match Belgium - Italy, Italy wo
 uefaEuro2016(['Portugal', 'Iceland'],[1, 1]) // "At match Portugal - Iceland, teams played draw."
 */
 
+function uefaEuro2016([t1,t2], [s1,s2]){
+  if (s1 == s2) {
+    return `At match ${t1} - ${t2}, teams played draw.`;
+    }
+  if (s2 > s1) {
+    return `At match ${t1} - ${t2}, ${t2} won!`;
+    }
+  if (s2 < s1) {
+    return `At match ${t1} - ${t2}, ${t1} won!`;
+    }
+}
