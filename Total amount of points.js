30e2ece5abf4a48c6187d7b0518114e756619b7a
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-11-09 20:37:38 -0300

    Update Total amount of points.js

--- Total amount of points.js
+++ Total amount of points.js
@@ -31,4 +31,11 @@ function points(games) {
   return total;
 }
 
+/*
+Short solution:
 
+const points = games => games.reduce((output,current) => {
+    return output += current[0] > current[2] ? 3 : current[0] === current[2] ? 1 : 0;
+},0)
+
+*/
