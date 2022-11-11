47509868d30db5e02c1b65f1531a01d321eda377
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-11-11 20:41:45 -0300

    Update Syntax Error_ Array functions.js

--- Syntax Error_ Array functions.js
+++ Syntax Error_ Array functions.js
@@ -4,3 +4,18 @@ A dumb error I had that actually costed me 15 minutes.. since I cannot copy and 
 
 After finding this error, I felt that I had to share it with the world..
 */
+
+function Foo() {
+    this.arrOfFuncs = [
+        function(num) {
+            return parseInt(num, 2);
+        },
+        function(num) {
+            return parseInt(num, 8);
+        }
+    ];
+}
+
+Foo.prototype.convert = function(num, base) {
+    return this.arrOfFuncs[base](num);
+};
