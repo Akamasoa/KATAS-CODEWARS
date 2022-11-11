ba11a26f9883efd95284d4d2de89bbcc10261ee6
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-11-11 20:41:24 -0300

    Create Syntax Error_ Array functions.js

---
+++ Syntax Error_ Array functions.js
@@ -0,0 +1,6 @@
+/*
+Description:
+A dumb error I had that actually costed me 15 minutes.. since I cannot copy and paste my code, enjoy this contrived recreation of the problem.
+
+After finding this error, I felt that I had to share it with the world..
+*/
