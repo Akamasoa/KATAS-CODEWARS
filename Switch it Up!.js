743d3aec472e8495b6619d3e68333221ee09be36
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-11-12 20:34:10 -0300

    Update Switch it Up!.js

--- Switch it Up!.js
+++ Switch it Up!.js
@@ -11,3 +11,4 @@ Try using "Switch" statements.
 This kata is meant for beginners. Rank and upvote to bring it out of beta
 */
 
+switchItUp = n => ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"][n];
