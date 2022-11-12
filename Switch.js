11fb425963e258918c6a6814be886ea32f3205a5
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-11-12 20:31:07 -0300

    Create Switch.js

---
+++ Switch.js
@@ -0,0 +1,7 @@
+/*
+Description:
+Write a function caseInSwitch which uses a switch statement to test the variable val and sets the variable answer according to the following values of val: 1 - "alpha" 2 - "beta" 3 - "gamma" 4 - "delta"
+
+Then, test caseInSwitch for the argument 1 and print the value of answer to the console.
+*/
+
