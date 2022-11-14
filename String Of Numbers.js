51d9e6aa9c46b6051bb26c7cad132beb46399bc1
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-11-13 22:34:28 -0300

    Create String Of Numbers.js

---
+++ String Of Numbers.js
@@ -0,0 +1,9 @@
+/*
+Description:
+Given the arguments of two numbers, return a string of whole numbers between them, exclusive.
+
+The output should be a string of increasing numbers.
+
+Return an empty string if there are no numbers in between.
+*/
+
