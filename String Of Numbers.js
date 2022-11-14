c0f502cf94970223f880601791f55f252c9f9d3a
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-11-13 22:35:52 -0300

    Update String Of Numbers.js

--- String Of Numbers.js
+++ String Of Numbers.js
@@ -7,3 +7,12 @@ The output should be a string of increasing numbers.
 Return an empty string if there are no numbers in between.
 */
 
+let strOfNums = (num1, num2) => {
+  let start = Math.min(num1,num2);
+  let end = Math.max(num1,num2);
+  let str = '';
+  for (let i = start + 1; i < end; i++){
+  str+=i;
+  }
+  return str;
+}
