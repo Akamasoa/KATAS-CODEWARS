8f86c85d91129a038f6396bfe4510a76d0bb4646
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-11-17 19:37:03 -0300

    Update Reversed Strings.js

--- Reversed Strings.js
+++ Reversed Strings.js
@@ -9,4 +9,14 @@ function solution(str){
   return str.split('').reverse().join('');  
 }
 
+/*Long solution:
 
+function solution(s){
+  var o = '';
+  for (var i = s.length - 1; i >= 0; i--){
+    o += s[i];
+  }
+  return o;
+}
+
+*/
