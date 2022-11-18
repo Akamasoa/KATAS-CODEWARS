cad4eb3769cff065207e22ccc5517c7abd08702d
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-11-18 20:37:48 -0300

    Update Replace values with strings.js

--- Replace values with strings.js
+++ Replace values with strings.js
@@ -10,3 +10,19 @@ printValues(0)  // -> []
 printValues(10) // -> [1,2,3,4,'Wars',6,'Code',8,9,'Wars']
 */
 
+function printValues(n){
+  let arr = [];
+  for (let i = 1; i <= n; i++){
+    if (i % 5 === 0 && i % 7 === 0){
+      arr.push("CodeWars");
+    }
+    else if (i % 5 === 0){
+      arr.push("Wars");
+    }
+    else if (i % 7 === 0){
+      arr.push("Code");
+    }
+    else arr.push(i);
+  }
+  return arr;
+}
