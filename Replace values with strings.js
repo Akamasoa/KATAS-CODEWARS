c7022ebe3243fb95b64dd171f5899efbaf48313a
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-11-18 20:36:36 -0300

    Create Replace values with strings.js

---
+++ Replace values with strings.js
@@ -0,0 +1,12 @@
+/*
+Write a function printValues(n) that returns an array of all the numbers from 1 to n, with two exceptions:
+
+For numbers divisible by 7, print "Code" instead of the number
+and for numbers divisible by 5, print "Wars" instead.
+("CodeWars" should be added to the array for numbers divisible by 7 and 5)
+
+Example:
+printValues(0)  // -> []
+printValues(10) // -> [1,2,3,4,'Wars',6,'Code',8,9,'Wars']
+*/
+
