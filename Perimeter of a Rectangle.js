6ebc21d873a311c9caf5317b111030c0a077f3d8
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-11-22 22:22:09 -0300

    Update Perimeter of a Rectangle.js

--- Perimeter of a Rectangle.js
+++ Perimeter of a Rectangle.js
@@ -5,3 +5,10 @@ Complete the method to calculate the perimeter of a rectangle using given width 
 You don't need to validate input parameters, all tests has valid arguments (positive integers).
 */
 
+var Kata = (function() {
+  function Kata() {}
+  Kata.getPerimeter = function(length, width) {
+    return 2 * (length + width);
+  };
+  return Kata;
+})();
