9a82828919cd3d0afe797b9ea47a3aed0bc31162
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-11-22 22:20:33 -0300

    Create Perimeter of a Rectangle.js

---
+++ Perimeter of a Rectangle.js
@@ -0,0 +1,7 @@
+/*
+Description:
+Complete the method to calculate the perimeter of a rectangle using given width and length.
+
+You don't need to validate input parameters, all tests has valid arguments (positive integers).
+*/
+
