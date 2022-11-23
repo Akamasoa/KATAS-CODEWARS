916c707b7d99d96b720bed3d9459a199d5e2057f
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-11-22 22:28:18 -0300

    Update Playing with cubes II.js

--- Playing with cubes II.js
+++ Playing with cubes II.js
@@ -9,3 +9,16 @@ Also we got a problem with negative values. Correct the code so negative values 
 The constructor taking no arguments should assign 0 to Cube's Side property.
 */
 
+class Cube {
+  constructor(side) {
+    this.setSide(side);
+  }
+
+  getSide() {
+    return this.side;
+  }
+  
+  setSide(side = 0) {
+    this.side = Math.abs(side);
+  }
+}
