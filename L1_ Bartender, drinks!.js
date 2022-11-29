c7007723a917e82198e75f8ca2c3db7e444f40c0
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-11-28 22:38:08 -0300

    Update L1_ Bartender, drinks!.js

--- L1_ Bartender, drinks!.js
+++ L1_ Bartender, drinks!.js
@@ -15,3 +15,23 @@ Note: anything else is the default case: if the input to the function is not any
 Make sure you cover the cases where certain words do not show up with correct capitalization. For example, getDrinkByProfession("pOLitiCIaN") should still return "Your tax dollars".
 */
 
+function getDrinkByProfession(param) {
+  param = param.toLowerCase();
+
+  switch (param) {
+    case "jabroni":
+      return "Patron Tequila";
+    case "school counselor":
+      return "Anything with Alcohol";
+    case "programmer":
+      return "Hipster Craft Beer";
+    case "bike gang member":
+      return "Moonshine";
+    case "politician":
+      return "Your tax dollars";
+    case "rapper":
+      return "Cristal";
+    default:
+      return "Beer";
+  }
+}
