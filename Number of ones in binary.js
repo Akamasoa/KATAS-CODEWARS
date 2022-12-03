f7e3590163b3065468cccec2d6f2dfe7c0b22aaf
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-12-03 19:21:01 -0300

    Create Number of ones in binary.js

---
+++ Number of ones in binary.js
@@ -0,0 +1,12 @@
+/*
+Description:
+Write a function which takes a positive integer and returns the number of ones that appear in its binary representation.
+
+For example,
+ones(1); // -> 1
+ones(4); // -> 1
+ones(7); // -> 3
+
+You may assume that the input is always a positive integer.
+*/
+
