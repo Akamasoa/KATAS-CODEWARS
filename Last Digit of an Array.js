82625120501a667374076bf9e88fa83d02c99648
Franco Sebastián Genre <84336216+francogenre@users.noreply.github.com>
2022-12-06 23:00:04 -0300

    Create Last Digit of an Array.js

---
+++ Last Digit of an Array.js
@@ -0,0 +1,11 @@
+/*
+Description:
+Write a function that returns the last digit of rightmost integer in the given array. Be careful - there might be numbers that aren't integers in the arrays!
+
+[1,2,3] --> 3
+["a", 45, "b", 67, "c"] --> 7
+[-17, -4, -12] --> 2
+["g", 12, "h", 4.5, "i"] --> 2
+["a", "b", "c"] --> "No integers found."
+*/
+
